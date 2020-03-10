d6d6093e3c6d95e341e2b607adafdc8ad72d7f33
meliabt19 <58869913+meliabt19@users.noreply.github.com>
2020-03-09 21:08:59 -0400

    Generater Fix

--- README.md
+++ README.md
@@ -47,3 +47,54 @@ You are required to submit the following for review:
 
 - - -
 © 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Password Generator</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="wrapper">
+      <header>
+        <h1>Password Generator</h1>
+      </header>
+      <div class="card">
+        <div class="card-header">
+          <h2>Generate a Password</h2>
+        </div>
+        <div class="card-body">
+          <textarea
+            readonly
+            id="password"
+            placeholder="Your Secure Password"
+            aria-label="Generated Password"
+          ></textarea>
+        </div>
+        <div class="card-footer">
+          <button id="generate" class="btn">Generate Password</button>
+        </div>
+      </div>
+    </div>
+    <script src="script.js"></script>
+  </body>
+</html>
--- script.js
+++ script.js
@@ -1,14 +1,95 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+window.addEventListener('load', function() {
+  var parameters = prompt("How many characters would you like your password to be?");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  while (parameters < 8 || parameters > 128) {
+    parameters = prompt("Length must be 8-128 characters. How many characters would you like your password to be?");
+  }
 
-  passwordText.value = password;
+  var uppercase = confirm("Would you like to use uppercase letters?");
+  var lowercase = confirm("Would you like to use lowercase letters?");
+  var numbers = confirm("would you like to use numbers?");
+  var symbols = confirm("would you like to use special characters?");
 
+  while (!(uppercase || lowercase || numbers || symbols)) {
+    alert("You must select at least one character type!");
+
+    uppercase = confirm("Would you like to use uppercase letters?");
+    lowercase = confirm("Would you like to use lowercase letters?");
+    numbers = confirm("would you like to use numbers?");
+    symbols = confirm("would you like to use special characters?");
+  }
+
+  //DOM elements
+  const resultEl = document.getElementById('password');
+
+  document.getElementById('generate').addEventListener('click', () => {
+    resultEl.value = generatePassword(lowercase, uppercase, numbers, symbols, parameters);
+  });
+
+  document.getElementById('clipboard').addEventListener('click', () => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.value;
+
+    if (!password) {
+      return;
+    }
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert('Password copied to clipboard');
+  });
+});
+
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = '';
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{
+    lower
+  }, {
+    upper
+  }, {
+    number
+  }, {
+    symbol
+  }].filter(item => Object.values(item)[0]);
+
+  // create a loop
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Generator functions
+function getRandomLower() {
+  return rando("qwertyuiopasdfghjklzxcvbnm")
+}
+
+function getRandomUpper() {
+  return rando("QWERTYUIOPASDFGHJKLZXCVBNM");
+}
+
+function getRandomNumber() {
+  return rando(9);
+}
+
+function getRandomSymbol() {
+  return rando('!@#$%^&*(){}[]=<>/,.');
+}
